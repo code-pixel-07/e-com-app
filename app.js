acd33fcab44df44f89b1ab43445610f91df3123e
code-pixel-07 <sharonshaji127001@gmail.com>
2020-11-07 20:04:26 +0530

    Commit-2: added bootstrap, modified default stylesheet, created homepage as in index.hbs, modified layout.hbs, used express handlebar module, created partials for admin and user.

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hbs = require('express-handlebars')
 
-var indexRouter = require('./routes/index');
+var adminRouter = require('./routes/admin');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -12,6 +13,15 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.engine('hbs', hbs(
+  {
+    extname:'hbs', 
+    defaultLayout:'layout',
+    layoutsDir:__dirname + '/views/layouts/',
+    partialsDir:__dirname + '/views/partials/'
+  }
+))
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +29,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/admin', adminRouter);
+app.use('/', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,33 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  const Products = [
+    {
+      name:'Redmi Note 9 pro',
+      categoty:'Smartphones',
+      description:'Lorem ipsum dolor sit amet, adipisicing elit. Maxime quia voluptatum ab.',
+      image:'https://images-na.ssl-images-amazon.com/images/I/81mtDLql%2BXL._SX342_.jpg'
+    },
+    {
+      name:'Realme Narzo',
+      categoty:'Smartphones',
+      description:'Lorem ipsum dolor sit amet, adipisicing elit. Maxime quia voluptatum ab.',
+      image:'https://images-na.ssl-images-amazon.com/images/I/71bp9IpcK-L._SX679_.jpg'
+    },
+    {
+      name:'Vivo X50 Pro',
+      categoty:'Smartphones',
+      description:'Lorem ipsum dolor sit amet, adipisicing elit. Maxime quia voluptatum ab.',
+      image:'https://images-na.ssl-images-amazon.com/images/I/71hrhKdmwDL._SX522_.jpg'
+    },
+    {
+      name:'HP Pavilion gaming laptop',
+      categoty:'Laptops',
+      description:'Lorem ipsum dolor sit amet, adipisicing elit. Maxime quia voluptatum ab.',
+      image:'https://images-na.ssl-images-amazon.com/images/I/81pezrPSgOL._SX569_.jpg'
+    },
+  ]
+  res.render('index', {Products, admin:false});
 });
 
 module.exports = router;
